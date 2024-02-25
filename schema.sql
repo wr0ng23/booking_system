b030ede61a4c852ea859e320868d0f530f324cdd
wr0ng23 <kolya.petrow38@gmail.com>
2024-02-25 21:08:35 +0300

    Add functionality for creating new ads and watch list of already created ads

--- schema.sql
+++ schema.sql
@@ -1,4 +1,24 @@
-CREATE TABLE users (
-                       id           bigint,
-                       name_of_user varchar
+CREATE TABLE users
+(
+    id           bigint primary key,
+    state_of_user varchar,
+    name_of_user varchar
+);
+
+CREATE TABLE orders
+(
+    id serial primary key ,
+    id_of_user  bigint,
+    number_of_order bigint,
+    description varchar,
+    foreign key (id_of_user) references users (id),
+    unique (id_of_user, number_of_order)
+);
+
+CREATE TABLE photos_of_order
+(
+    id varchar primary key,
+    id_of_order bigint,
+    foreign key (id_of_order) references orders (id),
+    unique (id)
 );
