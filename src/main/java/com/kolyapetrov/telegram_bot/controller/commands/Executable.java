9ed89118b9f4a75be98b6eaebfbb1ae5cb5e8666
Kolya <36518599+wr0ng23@users.noreply.github.com>
2024-04-26 21:14:19 +0300

    Delete src/main/java/com/kolyapetrov/telegram_bot/controller/commands/Executable.java

--- src/main/java/com/kolyapetrov/telegram_bot/controller/commands/Executable.java
+++
@@ -1,9 +0,0 @@
-package com.kolyapetrov.telegram_bot.controller.commands;
-
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-//TODO: maybe make /exit command for clear all data about user
-public interface Executable {
-    SendMessage retrieveMessage(Update update);
-}
